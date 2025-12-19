d4b2102b299c037ea6bb689ca20016b89ce79883
hirausman101 <221406@students.au.edu.pk>
2025-12-19 13:24:03 +0500

    Added folders

---
+++ profile/config.php
@@ -0,0 +1,11 @@
+<?php
+ $dbhost = 'localhost';
+            $dbuser = 'hira';
+            $dbpass = 'hiramalik)@';
+            $dbname = 'wordpressone';
+			$dbport = '3308';
+
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname,$dbport) or die('connection failed');
+
+?>
---
+++ profile/login.php
@@ -0,0 +1,60 @@
+<?php
+
+include 'config.php';
+
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, $_POST['password']);
+
+
+   $select = mysqli_query($conn, "SELECT * FROM `profile_signup` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_email'] = $row['email'];
+      header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.txt">
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3>login now</h3>
+      <?php
+      if(isset($message)){
+         foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+         }
+      }
+      ?>
+      <input type="email" name="email" placeholder="enter email" class="box" required>
+      <input type="password" name="password" placeholder="enter password" class="box" required>
+      <input type="submit" name="submit" value="login now" class="btn">
+      <p>don't have an account? <a href="register.php">regiser now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
